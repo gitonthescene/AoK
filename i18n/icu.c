8c0e6c180e8a46e89905036bfb8eae36d49827aa
Douglas Mennella <douglas.mennella@gmail.com>
2025-09-25 20:52:40 +0900

    cleaner

--- i18n/icu.c
+++ i18n/icu.c
@@ -4,24 +4,28 @@
 #include"../../k.h"
 
 K Fup(K x){
-  K r=KC(0,256);UChar bi[256],bo[256];N l=NK(r);
+  N l=NK(x);
+  K r=KC(0,4*l);UChar bi[4*(1+l)],bo[4*(1+l)];
   UErrorCode e=U_ZERO_ERROR;
-  u_strFromUTF8(bi,sizeof(bi)/sizeof(bi[0]),0,(S)x,NK(x),&e);
+  u_strFromUTF8(bi,sizeof(bi)/sizeof(bi[0]),0,(S)x,l,&e);
+  unref(x);
   if(U_FAILURE(e)) {*(C*)r=0; return r;}
-  N sz=u_strToUpper(bo,sizeof(bo)/sizeof(bo[0]),bi,NK(x),0,&e);
-  if(sz>l||U_FAILURE(e)){*(C*)r=0; return r;}
+  N sz=u_strToUpper(bo,sizeof(bo)/sizeof(bo[0]),bi,l,0,&e);
+  if(sz!=l||U_FAILURE(e)){*(C*)r=0; return r;}
   u_strToUTF8((C*)r,NK(r),0,bo,sz,&e);
   if(U_FAILURE(e)) {*(C*)r=0;}
   return KN(r,sz);
 }
 
 K Flow(K x){
-  K r=KC(0,256);UChar bi[256],bo[256];N l=NK(r);
+  N l=NK(x);
+  K r=KC(0,4*l);UChar bi[4*(1+l)],bo[4*(1+l)];
   UErrorCode e=U_ZERO_ERROR;
-  u_strFromUTF8(bi,sizeof(bi)/sizeof(bi[0]),0,(S)x,NK(x),&e);
+  u_strFromUTF8(bi,sizeof(bi)/sizeof(bi[0]),0,(S)x,l,&e);
+  unref(x);
   if(U_FAILURE(e)) {*(C*)r=0; return r;}
-  N sz=u_strToLower(bo,sizeof(bo)/sizeof(bo[0]),bi,NK(x),0,&e);
-  if(sz>l||U_FAILURE(e)){*(C*)r=0; return r;}
+  N sz=u_strToLower(bo,sizeof(bo)/sizeof(bo[0]),bi,l,0,&e);
+  if(sz!=l||U_FAILURE(e)){*(C*)r=0; return r;}
   u_strToUTF8((C*)r,NK(r),0,bo,sz,&e);
   if(U_FAILURE(e)) {*(C*)r=0;}
   return KN(r,sz);
