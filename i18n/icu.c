262b99f246fd2ec2cd723dfb60ccaafb09e54400
Douglas Mennella <douglas.mennella@gmail.com>
2025-09-26 13:00:42 +0900

    more stable (ensure null terminator)

--- i18n/icu.c
+++ i18n/icu.c
@@ -4,11 +4,10 @@
 #include"../../k.h"
 
 K Fup(K x){
-  N l=NK(x);
+  N l=NK(x);C b[l+1];CK(b,x);b[l]=0;
   K r=KC(0,4*l);UChar bi[4*(1+l)],bo[4*(1+l)];
   UErrorCode e=U_ZERO_ERROR;
-  u_strFromUTF8(bi,sizeof(bi)/sizeof(bi[0]),0,(S)x,l,&e);
-  unref(x);
+  u_strFromUTF8(bi,sizeof(bi)/sizeof(bi[0]),0,b,l,&e);
   if(U_FAILURE(e)) {*(C*)r=0; return r;}
   N sz=u_strToUpper(bo,sizeof(bo)/sizeof(bo[0]),bi,l,0,&e);
   if(sz!=l||U_FAILURE(e)){*(C*)r=0; return r;}
@@ -18,11 +17,10 @@ K Fup(K x){
 }
 
 K Flow(K x){
-  N l=NK(x);
+  N l=NK(x);C b[l+1];CK(b,x);b[l]=0;
   K r=KC(0,4*l);UChar bi[4*(1+l)],bo[4*(1+l)];
   UErrorCode e=U_ZERO_ERROR;
-  u_strFromUTF8(bi,sizeof(bi)/sizeof(bi[0]),0,(S)x,l,&e);
-  unref(x);
+  u_strFromUTF8(bi,sizeof(bi)/sizeof(bi[0]),0,b,l,&e);
   if(U_FAILURE(e)) {*(C*)r=0; return r;}
   N sz=u_strToLower(bo,sizeof(bo)/sizeof(bo[0]),bi,l,0,&e);
   if(sz!=l||U_FAILURE(e)){*(C*)r=0; return r;}
