fc79caf14197d4e9b19418ec76f70130a381bd54
Douglas Mennella <douglas.mennella@gmail.com>
2025-09-29 11:22:53 +0900

    more work on plugin

--- i18n/icu.c
+++ i18n/icu.c
@@ -1,32 +1,59 @@
 // ICU FFI to growler/k
 
 #include "unicode/ustring.h"
-#include"../../k.h"
+#include "unicode/utrans.h"
+#include "../../k.h"
+#include "stdio.h"
+#define MAX(x,y) ({typeof(x) x_=(x),y_=(y);x_>y_?x_:y_;})
 
-K Fup(K x){
-  N l=NK(x);C b[l+1];CK(b,x);b[l]=0;
-  K r=KC(0,4*l);UChar bi[4*(1+l)],bo[4*(1+l)];
-  UErrorCode e=U_ZERO_ERROR;
-  u_strFromUTF8(bi,sizeof(bi)/sizeof(bi[0]),0,b,-1,&e);
-  if(U_FAILURE(e)) {*(C*)r=0; return r;}
-  I so;N sz=u_strToUpper(bo,sizeof(bo)/sizeof(bo[0]),bi,l,0,&e);
+V UK(K x,UChar* b,N n,I* s,UErrorCode* e){
+  N l=NK(x);C bc[l+1];CK(bc,x);bc[l]=0;
+  u_strFromUTF8(b,n,s,bc,-1,e);
+  if(U_FAILURE(*e)){
+	printf("no go: %s\n",bc);
+  }
+}
+
+K Ftrns(K x,K y){
+  UErrorCode e=U_ZERO_ERROR;UParseError pe;
+  N l=MAX(NK(x),NK(y));K r=KC(0,2*l);UChar b[l+1];I s;
+  UK(x,b,l+1,&s,&e);
+  UTransliterator* t=utrans_openU(b,-1,UTRANS_FORWARD,0,0,&pe,&e);
+  if(!t){*(C*)r=0; return r;}
+  UK(y,b,l+1,&s,&e);
+  if(U_FAILURE(e)){utrans_close(t); *(C*)r=0; return r;}
+  I til=s;
+  // Assume one shot
+  utrans_transUChars(t,b,0,l+1,0,&til,&e);
+  utrans_close(t);
   if(U_FAILURE(e)){*(C*)r=0; return r;}
-  u_strToUTF8((C*)r,NK(r),&so,bo,-1,&e);
+  I so;u_strToUTF8((C*)r,NK(r),&so,b,-1,&e);
   if(U_FAILURE(e)) {*(C*)r=0;}
   return KN(r,so);
 }
 
-K Flow(K x){
-  N l=NK(x);C b[l+1];CK(b,x);b[l]=0;
-  K r=KC(0,4*l);UChar bi[4*(1+l)],bo[4*(1+l)];
-  UErrorCode e=U_ZERO_ERROR;
-  u_strFromUTF8(bi,sizeof(bi)/sizeof(bi[0]),0,b,-1,&e);
+K Fup(K x){
+  N l=NK(x);UErrorCode e=U_ZERO_ERROR;
+  K r=KC(0,l+1);UChar bi[1+l],bo[1+l];I s;
+  UK(x,bi,l+1,&s,&e);
   if(U_FAILURE(e)) {*(C*)r=0; return r;}
-  I so;N sz=u_strToLower(bo,sizeof(bo)/sizeof(bo[0]),bi,l,0,&e);
+  u_strToUpper(bo,l+1,bi,l,0,&e);
   if(U_FAILURE(e)){*(C*)r=0; return r;}
-  u_strToUTF8((C*)r,NK(r),&so,bo,-1,&e);
+  u_strToUTF8((C*)r,NK(r),&s,bo,-1,&e);
   if(U_FAILURE(e)) {*(C*)r=0;}
-  return KN(r,so);
+  return KN(r,s);
+}
+
+K Flow(K x){
+  N l=NK(x);UErrorCode e=U_ZERO_ERROR;
+  K r=KC(0,l+1);UChar bi[1+l],bo[1+l];I s;
+  UK(x,bi,l+1,&s,&e);
+  if(U_FAILURE(e)) {*(C*)r=0; return r;}
+  u_strToLower(bo,l+1,bi,l,0,&e);
+  if(U_FAILURE(e)){*(C*)r=0; return r;}
+  u_strToUTF8((C*)r,NK(r),&s,bo,-1,&e);
+  if(U_FAILURE(e)) {*(C*)r=0;}
+  return KN(r,s);
 }
 
 __attribute__((constructor)) void loadicu(K);
@@ -34,4 +61,5 @@ void loadicu(K k) {
   kinit();
  KR("icuup", (void*)Fup, 1);
  KR("iculow", (void*)Flow, 1);
+ KR("icutrans", (void*)Ftrns, 2);
 }
